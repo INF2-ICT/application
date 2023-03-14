ef035644404ee1bc8d06319a94fef6fcd625f194
robin <robenjay@hotmail.nl>
2023-03-14 13:38:22 +0100

    file validation checking if fields are there and docx pdf etc fileformat checked

--- quintorApplication/src/main/java/com/example/quintorapplication/controllers/Mt940FileAddController.java
+++ quintorApplication/src/main/java/com/example/quintorapplication/controllers/Mt940FileAddController.java
@@ -23,20 +23,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Mt940FileAddController {
 
     @FXML
     public Button uploadFile;
-
-    private ArrayList<Boolean> checkList;
-
     private Stage stage;
-
-    public Mt940FileAddController(ArrayList<Boolean> checkList) {
-        this.checkList = checkList;
+    private ArrayList<Boolean> checkList;
+    public Mt940FileAddController() {
+        this.checkList = new ArrayList<>();
+    }
+    private void addCheckList(boolean correct) {
+        this.checkList.add(correct);
     }
 
+    private void emptyCheckList() {
+        this.checkList.clear();
+    }
+
+    private ArrayList<Boolean> getCheckList() {
+        return this.checkList;
+    }
     public void switchToDashboard(MouseEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StarterApplication.class.getResource("dashboard/dashboard-view.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
@@ -50,12 +58,23 @@ public class Mt940FileAddController {
         FileChooser fileChooser = new FileChooser();
         File response = fileChooser.showOpenDialog(null);
 
+        boolean fileChecked = false;
+
+
         if (response != null) {
 
-            String text = Files.readString(Paths.get(response.toURI()));
+            Scanner sc = new Scanner(response);
 
-            System.out.println(validateMT940(text));
+            while (sc.hasNextLine()) {
+                if (sc.nextLine().length() > 0) {
+                    fileChecked = true;
+                }
+            }
 
+            if (fileChecked) {
+                String text = Files.readString(Paths.get(response.toURI()));
+                System.out.println(validateMT940(text));
+            }
         }
 
         return null;
@@ -64,8 +83,10 @@ public class Mt940FileAddController {
     private boolean validateMT940 (String file) {
 
         MT940 mt = new MT940(file);
+        System.out.println("test");
 
         if (mt.getSwiftMessage().getBlock4() != null) {
+            System.out.println("got through?");
             Tag start = mt.getSwiftMessage().getBlock4().getTagByNumber(20);
             Tag end = mt.getSwiftMessage().getBlock4().getTagByNumber(62);
 
@@ -73,35 +94,44 @@ public class Mt940FileAddController {
 
             for (int i = 0; i < loop.size(); i++) {
                 Tag t = loop.getTag(i);
-
-                if (t.getName().equals("20")) {
-                    Field20 tx = (Field20) t.asField();
-                    System.out.println(tx);
-                    checkList.add(true);
-                }
-
-                if (t.getName().equals("25")) {
-                    Field25 tx = (Field25) t.asField();
-                    System.out.println(tx);
-                    checkList.add(true);
-                }
-
-                if (t.getName().equals("60F")) {
-                    Field60F tx = (Field60F) t.asField();
-                    System.out.println(tx);
-                    checkList.add(true);
-                }
-
-                if (t.getName().equals("62F")) {
-                    Field62F tx = (Field62F) t.asField();
-                    System.out.println(tx);
-                    checkList.add(true);
-                }
-
+                CheckField20(t);
+                CheckField25(t);
+                CheckField60F(t);
+                CheckField62F(t);
             }
         }
 
+        System.out.println(getCheckList());
+
         return true;
+    }
+    private void CheckField20(Tag t) {
+        if (t.getName().equals("20")) {
+            Field20 tx = (Field20) t.asField();
+            System.out.println(tx);
+            addCheckList(true);
+        }
+    }
+    private void CheckField25(Tag t) {
+        if (t.getName().equals("25")) {
+            Field25 tx = (Field25) t.asField();
+            System.out.println(tx);
+            addCheckList(true);
+        }
+    }
+    private void CheckField60F(Tag t) {
+        if (t.getName().equals("60F")) {
+            Field60F tx = (Field60F) t.asField();
+            System.out.println(tx);
+            addCheckList(true);
+        }
+    }
+    private void CheckField62F(Tag t) {
+        if (t.getName().equals("62F")) {
+            Field62F tx = (Field62F) t.asField();
+            System.out.println(tx);
+            addCheckList(true);
+        }
     }
 
 }
